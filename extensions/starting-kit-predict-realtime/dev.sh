e06b308ba1b19789cd121618600a397b2a0871c8
yebowen <yebowen@4paradigm.com>
2024-04-16 15:17:13 +0800

    introduce openmldb

---
+++ extensions/starting-kit-predict-realtime/dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+#修改imgname为你自己的镜像名后，注释掉本行内容
+imgname="demo.test/automl:0.0.1"
+docker rm -f automl
+docker rmi -f $imgname
+docker build -t $imgname .
+docker run -it --name automl -p 80:80 $imgname
